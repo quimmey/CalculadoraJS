231c1ed2ad983201740f8268b91865006aba4905
quimmey <quimey98@gmail.com>
2022-05-26 20:37:28 -0300

    html&css final

---
+++ js/scrpit.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', ()=> {
+    const display = document.querySelector ('calculator-display');
+    const keypadButtons = document.getElementsByClassName('keypad-button')
+})
