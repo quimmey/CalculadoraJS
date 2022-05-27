a22dc03a26226be89826d8cccbab74888bff4377
quimmey <quimey98@gmail.com>
2022-05-26 23:57:19 -0300

    create js

--- js/scrpit.js
+++ js/scrpit.js
@@ -1,4 +1,41 @@
 window.addEventListener('load', ()=> {
-    const display = document.querySelector ('calculator-display');
-    const keypadButtons = document.getElementsByClassName('keypad-button')
-})
+    const display = document.querySelector ('.calculator-display');
+    const keypadButtons = document.getElementsByClassName('keypad-button');
+
+    const keypadButtonsArray = Array.from(keypadButtons);
+
+    keypadButtonsArray.forEach( (button) => {
+        
+        button.addEventListener('click', ()=> {
+
+        })
+    })
+});
+
+
+function calculadora(button, display) {
+    switch (button.innerHTML) {
+        case 'C':
+            borrar(display);
+            break;
+        case '=':
+            calcular(display);
+            break;
+        default
+            actualizar(display, button);
+            break;
+    }
+
+}
+
+function calcular(display) {
+
+    display.innerHTML = eval(display.innerHTML)
+}
+
+function actualizar(display, button) {
+    if (display.innerHTML == 0) {
+        display.innetHTML = '';
+    }
+    display.innerHTML += button.innerHTML
+}
